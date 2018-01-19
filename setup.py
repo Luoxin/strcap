785736ebbd3a2a7fb3cd4a385a3f73c655ab182e
LuoxinY <33619903+LuoxinY@users.noreply.github.com>
2018-01-19 13:59:40 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+from distutils.core import setup
+setup(name='strcap',
+      version='0.0.0.1',
+      author='Luoxin',
+      description='Simple extension of string.',
+      platforms='Python2.X',
+      author_email='luoxin_T_T@outlook.com',
+      py_modules=['work01'],
+)
